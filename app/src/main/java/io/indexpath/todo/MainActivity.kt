3b01ee7ec0b3aa1b54eeed4e46374ee2ba285698
yuki.pro <yuki.kr@gmail.com>
2018-06-20 17:37:37 +0900

    스플래쉬

--- app/src/main/java/io/indexpath/todo/MainActivity.kt
+++ app/src/main/java/io/indexpath/todo/MainActivity.kt
@@ -2,11 +2,24 @@ package io.indexpath.todo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+
+//        try {
+//            supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+//            supportActionBar!!.setCustomView(R.layout.custom_bar)
+//        } catch (e: Exception) {
+//            println(e.message)
+//        }
+
+
         setContentView(R.layout.activity_main)
     }
 }
